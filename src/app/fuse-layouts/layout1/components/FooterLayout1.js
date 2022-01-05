dbe12548163b7d6ef96295cc90d03c34b5257efc
Daniel Wust <danielwust99@gmail.com>
2022-01-05 00:07:00 -0300

    action

--- src/app/fuse-layouts/layout1/components/FooterLayout1.js
+++ src/app/fuse-layouts/layout1/components/FooterLayout1.js
@@ -9,18 +9,18 @@ import AppBar from "@material-ui/core/AppBar";
 
 function FooterLayout1(props) {
     const footerTheme = useSelector(selectFooterTheme);
+		const sendCommand = () => console.log(document.getElementById("command").value);
 
     return (
 			<ThemeProvider theme={footerTheme}>
 				<AppBar
 					id="fuse-footer" className="relative z-10 shadow-md" color="default"
-					style={{
-							backgroundColor: footerTheme.palette.background.paper,
-					}}>
+					style={{ backgroundColor: footerTheme.palette.background.paper, cursor: "pointer" }}>
 					<Typography className="mx-10 my-8 flex items-center justify-between">
 							<input id="command" type="text" name="command" placeholder="Type your command here... or not, it's up to you."
-								className="px-10 py-6 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 w-full" />
-							<ArrowUpwardSharp></ArrowUpwardSharp>
+								className="focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 
+								border border-gray-300 rounded-md shadow-sm disabled:bg-gray-100 mt-1 w-full px-10 py-6" />
+							<ArrowUpwardSharp onClick={sendCommand} className="mx-6"></ArrowUpwardSharp>
 					</Typography>
 				</AppBar>
 			</ThemeProvider>
