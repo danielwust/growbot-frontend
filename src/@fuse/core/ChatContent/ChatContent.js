eb1a30a739fc47e6b8a8788a2f1146ced06e64e2
Daniel Wust <danielwust99@gmail.com>
2022-01-06 20:49:13 -0300

    simulando perguntas

--- src/@fuse/core/ChatContent/ChatContent.js
+++ src/@fuse/core/ChatContent/ChatContent.js
@@ -1,24 +1,58 @@
 import React from "react";
 
+const quests = [
+    {
+        name: {
+            quest: "Qual é o seu principal nome em cadastros?",
+            subquest: "Selecione uma das opções",
+            answers: [
+                "Nome",
+                "Sobrenome",
+                "Nome de batalha",
+                "Usuario de acesso",
+            ],
+            type: "multiple",
+        },
+    },
+    {
+        email: {
+            quest: "Qual o seu email?",
+            subquest: "Insira seu email",
+            answers: ["Email"],
+            type: "input",
+        },
+    },
+    {
+        date: {
+            quest: "Qual a sua data de nascimento?",
+            subquest: "Insira seu nascimento",
+            answers: ["Data de nascimento"],
+            type: "input",
+        },
+    },
+];
+
+let answers = [];
+quests[0].name.answers.map((answer, index) => {
+		answers.push(<p key={index}>{answer}</p>);
+}, this);
+
 function ChatContent() {
     return (
-			<div>
-				<img
-						src="assets/images/bot/flixbot.gif"	alt="bot"
-						style={{
-								maxWidth: "256px",
-								width: "25%",
-						}}
-						className="rounded-6"
-				/>
-				<h1 className="py-14">Pergunta</h1>
-				<h4 className="pb-12">Selecione uma das opções</h4>
-				<blockquote>
-					<p>Paragrafo 1</p>
-					<p>Paragrafo 2</p>
-					<p>Paragrafo 3</p>
-				</blockquote>
-			</div>
+        <div>
+            <img
+                src="assets/images/bot/flixbot.gif"
+                alt="bot"
+                style={{
+                    maxWidth: "256px",
+                    width: "25%",
+                }}
+                className="rounded-6"
+            />
+            <h1 className="py-14">{quests[0].name.quest}</h1>
+            <h4 className="pb-12">{quests[0].name.subquest}</h4>
+            <blockquote>{answers}</blockquote>
+        </div>
     );
 }
 
